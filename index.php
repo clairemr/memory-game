7827e98f5072f951a3f99706311b083b1a3a3d72
Claire Reincastle <clairemr@hotmail.com>
2020-04-06 11:53:18 +1000

    Setup php index page

---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">  
+  <title>JS Memory Game</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <script src="./script.js"></script>
+    <?php 
+    $cards = array("cactus", "peace lily", "jasmine", "lavender");
+    foreach($cards as $card){ ?>
+        <div class="card">
+            <img src="" class="front" alt="<?php echo $card; ?>">
+            <img src="" class="back" alt="back">
+        </div>
+
+        <?php
+    }
+    ?>
+    
+</body>
+</html>
