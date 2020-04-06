42600c01ab976227898246e542afa851cd453571
Claire Reincastle <clairemr@hotmail.com>
2020-04-06 13:32:59 +1000

    flip function now working

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const cards = document.querySelectorAll('.card');
 
 function flipCard(){
+    console.log(1);
     this.classList.toggle('flip');
+    console.log(2);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
